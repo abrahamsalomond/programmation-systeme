5aa499c95bf42c56e46f6a8cda258ae0ee978e96
Abraham Salomon <abrahamsalomon620@gmail.com>
2026-04-20 08:40:25 +0200

    correction de la question 1.1 du tp modifie

--- tp_1/corr_tp1.c
+++ tp_1/corr_tp1.c
@@ -10,10 +10,7 @@
 int main(void){
     char ligne[TAILLE_MAX];
     printf("myshell> ");
-    if(fgets(ligne, TAILLE_MAX, stdin) == NULL){
-        perror("Erreur de lecture de la ligne de commande");
-        return 1;}
-    printf("Vous avez entré: %s", ligne);
+    fgets(ligne, TAILLE_MAX, stdin);
     return 0;
 
 
